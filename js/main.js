43f98a833fd955bec05b840532fe355a54ced1ee
Andrii Litvinchuk <litvinchyk99@gmail.com>
2025-02-23 18:22:16 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import Header from "./Header.js";
+import TabsCollection from "./Tabs.js";
+
+import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js';
+
+new Header();
+new TabsCollection();
+
+defineScrollBarWidthCSSVar();
